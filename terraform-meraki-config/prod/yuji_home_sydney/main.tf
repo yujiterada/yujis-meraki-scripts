ef0a20f1f4e8010f056b509a7285ec88db7d4060
Yuji Terada <yuji.terada.jp@gmail.com>
2024-10-25 14:19:10 +1100

    move traffic shaping rules to base folder

--- terraform-meraki-config/prod/yuji_home_sydney/main.tf
+++ terraform-meraki-config/prod/yuji_home_sydney/main.tf
@@ -1,4 +1,4 @@
 module "traffic_shaping_rules" {
-  source = "../../modules/meraki/appliance/traffic_shaping_rules"
+  source = "../../modules/meraki/appliance/traffic_shaping_rules/base"
   network_id = var.network_id
 }
